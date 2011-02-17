fb33438e5b6f039b5577b883d26ef3cf1f013448
Henrik Johansson <dahankzter@gmail.com>
2011-02-17 20:33:52 +0100

    Initial Haskell attempt

---
+++ haskell/roman.hs
@@ -0,0 +1,27 @@
+module Main( main ) where
+
+import System.Environment
+import System( getArgs )
+import System.Console.GetOpt
+ 
+main = do 
+     args <- getArgs
+     let number = rInt (head args)
+     putStr ((times (number `div` 1000) "M") ++ 
+            (timesSmaller ((number `div` 100) `mod` 10) "C" "D" "M") ++
+            (timesSmaller ((number `div` 10) `mod` 10) "X" "L" "C") ++
+            (timesSmaller (number `mod` 10) "X" "V" "X") ++ "\n")
+
+times 0 _ = ""
+times n s = take n (cycle s)
+
+timesSmaller n a b c
+             | n <= 0 = ""
+             | n <= 3 = times n a
+             | n <= 4 = a ++ b
+             | n <= 8 = b ++ times (n-5) a
+             | n <= 9 = a ++ c
+             | otherwise = "Only single digits allowed!"
+
+rInt :: String -> Int
+rInt = read
