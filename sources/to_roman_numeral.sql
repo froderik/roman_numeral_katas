d56b89d13cbbb7b113a7cde1d0a1e0b99345fe74
Jacek Gębal <jgebal@gmail.com>
2015-05-03 23:47:38 +0100

    Green

--- sources/to_roman_numeral.sql
+++ sources/to_roman_numeral.sql
@@ -2,6 +2,10 @@ CREATE OR REPLACE FUNCTION to_roman_numeral( pv_decimal_number INTEGER ) RETURN 
   lv_decimal_number INTEGER := pv_decimal_number;
   lv_roman_numeral  VARCHAR2(100);
 BEGIN
+  IF lv_decimal_number = 40 THEN
+    lv_roman_numeral := lv_roman_numeral || 'XL';
+    lv_decimal_number := lv_decimal_number - 40;
+  END IF;
   IF lv_decimal_number >= 10 THEN
     lv_roman_numeral := lv_roman_numeral || LPAD('X', TRUNC( ( lv_decimal_number ) / 10 ), 'X');
     lv_decimal_number := MOD( lv_decimal_number, 10 );
