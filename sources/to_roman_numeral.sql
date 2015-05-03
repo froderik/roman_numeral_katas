b0e0aff16a10b2ef0c63518b49a159ba02e91dc1
Jacek Gębal <jgebal@gmail.com>
2015-05-03 23:41:53 +0100

    Deadend -> Refactor + Green

--- sources/to_roman_numeral.sql
+++ sources/to_roman_numeral.sql
@@ -1,36 +1,26 @@
 CREATE OR REPLACE FUNCTION to_roman_numeral( pv_decimal_number INTEGER ) RETURN VARCHAR2 IS
-
-  FUNCTION get_leading_ones( pv_decimal_number INTEGER , pv_range INTEGER ) RETURN VARCHAR2 IS
-    BEGIN
-      RETURN
-        CASE
-          WHEN pv_range = 10 AND MOD( pv_decimal_number, 10 ) = 9
-            OR pv_range =  5 AND MOD( pv_decimal_number, 10 ) = 4
-          THEN 'I'
-        END;
-    END;
-
-  FUNCTION get_v( pv_decimal_number INTEGER ) RETURN VARCHAR2 IS
-    BEGIN
-      RETURN
-        get_leading_ones( pv_decimal_number,  5 )
-        || CASE WHEN MOD( pv_decimal_number, 10 ) BETWEEN 4 AND  8 THEN  'V' END;
-    END;
-
-  FUNCTION get_x( pv_decimal_number INTEGER ) RETURN VARCHAR2 IS
-    BEGIN
-      RETURN
-        get_leading_ones( pv_decimal_number, 10 )
-        || TRIM( LPAD(' ', TRUNC( ( pv_decimal_number + 1 ) / 10 )+1, 'X') );
-    END;
-
-  FUNCTION get_trailing_ones( pv_decimal_number INTEGER ) RETURN VARCHAR2 IS
-    BEGIN
-      RETURN LPAD('I',MOD( MOD( MOD( pv_decimal_number, 5 ) , 4 ) - 1, 3 ) + 1, 'I' );
-    END;
-
+  lv_decimal_number INTEGER := pv_decimal_number;
+  lv_roman_numeral  VARCHAR2(100);
 BEGIN
-  RETURN get_x(pv_decimal_number) || get_v( pv_decimal_number ) || get_trailing_ones( pv_decimal_number );
+  IF lv_decimal_number >= 10 THEN
+    lv_roman_numeral := lv_roman_numeral || LPAD('X', TRUNC( ( lv_decimal_number ) / 10 ), 'X');
+    lv_decimal_number := MOD( lv_decimal_number, 10 );
+  END IF;
+  IF lv_decimal_number = 9 THEN
+    lv_roman_numeral := lv_roman_numeral || 'IX';
+  END IF;
+  IF lv_decimal_number BETWEEN 5 AND 8 THEN
+    lv_roman_numeral := lv_roman_numeral || 'V';
+    lv_decimal_number := lv_decimal_number - 5;
+  END IF;
+  IF lv_decimal_number = 4 THEN
+    lv_roman_numeral := lv_roman_numeral || 'IV';
+    lv_decimal_number := 0;
+  END IF;
+  IF lv_decimal_number BETWEEN 1 AND 3 THEN
+    lv_roman_numeral := lv_roman_numeral || LPAD('I',MOD( MOD( MOD( lv_decimal_number, 5 ) , 4 ) - 1, 3 ) + 1, 'I' );
+  END IF;
+  RETURN lv_roman_numeral;
 END;
 /
 
