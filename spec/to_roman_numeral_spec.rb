84718594598fce0a68086c097ac3fe1dcaa510c3
Jacek Gębal <jgebal@gmail.com>
2015-05-03 21:48:59 +0100

    Refactor

--- spec/to_roman_numeral_spec.rb
+++ spec/to_roman_numeral_spec.rb
@@ -2,12 +2,15 @@ require_relative 'spec_helper'
 
 describe 'Decimal to roman numeral converter' do
 
-  it 'should return I for 1' do
-    expect( plsql.to_roman_numeral(1) ).to eq('I')
+  [
+      [ 1,    'I'],
+      [ 2,   'II'],
+  ].each do |given, expected|
+    it "should return #{expected} for #{given}" do
+      expect( plsql.to_roman_numeral(given) ).to eq( expected )
+    end
+
   end
 
-  it 'should return II for 2' do
-    expect( plsql.to_roman_numeral(2) ).to eq('II')
-  end
 
 end
