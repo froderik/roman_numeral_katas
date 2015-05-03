032067f22135e6a0cf3f586b413aa7f23691488a
Jacek Gębal <jgebal@gmail.com>
2015-05-03 22:46:03 +0100

    Refactor

--- sources/to_roman_numeral.sql
+++ sources/to_roman_numeral.sql
@@ -1,18 +1,23 @@
 CREATE OR REPLACE FUNCTION to_roman_numeral( pv_decimal_number INTEGER ) RETURN VARCHAR2 IS
-BEGIN
-  RETURN
-    CASE pv_decimal_number
-      WHEN 1 THEN 'I'
-      WHEN 2 THEN 'II'
-      WHEN 3 THEN 'III'
-      WHEN 4 THEN 'IV'
-      WHEN 5 THEN 'V'
-      WHEN 6 THEN 'VI'
-      WHEN 7 THEN 'VII'
-      WHEN 8 THEN 'VIII'
-      WHEN 9 THEN 'IX'
-      WHEN 10 THEN 'X'
+
+  FUNCTION get_v_or_x( pv_decimal_number INTEGER ) RETURN VARCHAR2 IS
+    BEGIN
+      RETURN
+        CASE
+          WHEN MOD( pv_decimal_number,     10 )     BETWEEN 4 AND  8 THEN  'V'
+          WHEN MOD( pv_decimal_number - 1, 10 ) + 1 BETWEEN 9 AND 10 THEN  'X'
+        END;
     END;
+  FUNCTION get_leading_ones( pv_decimal_number INTEGER ) RETURN VARCHAR2 IS
+    BEGIN
+      RETURN CASE WHEN MOD( pv_decimal_number, 5 ) = 4 THEN  'I' END;
+    END;
+  FUNCTION get_trailing_ones( pv_decimal_number INTEGER ) RETURN VARCHAR2 IS
+    BEGIN
+      RETURN LPAD('I',MOD( MOD( MOD( pv_decimal_number, 5 ) , 4 ) - 1, 3 ) + 1, 'I' );
+    END;
+BEGIN
+  RETURN get_leading_ones( pv_decimal_number ) || get_v_or_x( pv_decimal_number ) || get_trailing_ones( pv_decimal_number );
 END;
 /
 
