d0ef261476f3ebbc08ba1a2b278212310ca75019
Jacek Gębal <jgebal@gmail.com>
2015-05-03 23:26:52 +0100

    Red

--- spec/to_roman_numeral_spec.rb
+++ spec/to_roman_numeral_spec.rb
@@ -2,8 +2,8 @@ require_relative 'spec_helper'
 
 describe 'Decimal to roman numeral converter' do
 
-  roman_numbers = %w{0 I II III IV V VI VII VIII IX X XI XII XIII XIV XV XVI XVII XVIII}
-  (1..18).each do |given|
+  roman_numbers = %w{0 I II III IV V VI VII VIII IX X XI XII XIII XIV XV XVI XVII XVIII XIX}
+  (1..19).each do |given|
     expected = roman_numbers[given]
     it "should return #{expected} for #{given}" do
       expect( plsql.to_roman_numeral(given) ).to eq( expected )
