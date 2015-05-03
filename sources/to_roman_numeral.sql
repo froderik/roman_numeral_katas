8d8a6cbb398386da9eff90a69f69c6f6e24d425d
Jacek Gębal <jgebal@gmail.com>
2015-05-04 00:43:23 +0100

    Refactor

--- sources/to_roman_numeral.sql
+++ sources/to_roman_numeral.sql
@@ -1,44 +1,42 @@
+CREATE OR REPLACE TYPE decimal_to_roman IS OBJECT (
+  divisor INTEGER,
+  symbol VARCHAR2(2),
+  MEMBER FUNCTION get_roman_numeral_piece(
+    pv_number IN OUT NOCOPY INTEGER
+  ) RETURN VARCHAR2
+);
+/
+
+CREATE OR REPLACE TYPE BODY decimal_to_roman IS
+  MEMBER FUNCTION get_roman_numeral_piece(
+    pv_number IN OUT NOCOPY INTEGER
+  ) RETURN VARCHAR2 IS
+    lv_result VARCHAR2(100);
+  BEGIN
+    IF pv_number >= divisor THEN
+      lv_result := LPAD( symbol, LENGTH( symbol ) * TRUNC( ( pv_number ) / divisor ) , symbol );
+      pv_number := MOD( pv_number, divisor );
+    END IF;
+    RETURN lv_result;
+  END;
+END;
+/
+
 CREATE OR REPLACE FUNCTION to_roman_numeral( pv_decimal_number INTEGER ) RETURN VARCHAR2 IS
   lv_decimal_number INTEGER := pv_decimal_number;
   lv_roman_numeral  VARCHAR2(100);
 
-  TYPE lt_decimal_roman_map_rec IS RECORD (
-    divisor INTEGER,
-    symbol VARCHAR2(2)
-  );
-  TYPE la_decimal_roman_map IS TABLE OF lt_decimal_roman_map_rec;
+  TYPE la_decimal_roman_map IS TABLE OF decimal_to_roman;
   lv_decimal_roman_map la_decimal_roman_map;
-
-  FUNCTION map( pv_divisor INTEGER, pv_symbol VARCHAR2 ) RETURN lt_decimal_roman_map_rec IS
-    lv_map_rec lt_decimal_roman_map_rec;
-    BEGIN
-      lv_map_rec.divisor := pv_divisor;
-      lv_map_rec.symbol := pv_symbol;
-      RETURN lv_map_rec;
-    END map;
-
-  FUNCTION build_roman_numeral_piece(
-    pv_map lt_decimal_roman_map_rec,
-    pv_decimal_number IN OUT NOCOPY INTEGER
-  ) RETURN VARCHAR2 IS
-    lv_roman_numeral  VARCHAR2(100);
-    BEGIN
-      IF pv_decimal_number >= pv_map.divisor THEN
-        lv_roman_numeral :=
-          LPAD( pv_map.symbol
-              , LENGTH( pv_map.symbol )
-                * TRUNC( ( pv_decimal_number ) / pv_map.divisor )
-              , pv_map.symbol);
-        pv_decimal_number := MOD( pv_decimal_number, pv_map.divisor );
-      END IF;
-      RETURN lv_roman_numeral;
-    END build_roman_numeral_piece;
 
 BEGIN
   lv_decimal_roman_map
-    := la_decimal_roman_map( map(90,'XC'), map(50,'L'), map(40,'XL'), map(10,'X'), map(9,'IX'), map(5,'V'), map(4,'IV'), map(1,'I') );
+    := la_decimal_roman_map(
+      decimal_to_roman(90,'XC'), decimal_to_roman(50,'L'), decimal_to_roman(40,'XL'), decimal_to_roman(10,'X'),
+      decimal_to_roman(9,'IX'), decimal_to_roman(5,'V'), decimal_to_roman(4,'IV'), decimal_to_roman(1,'I')
+      );
   FOR i IN lv_decimal_roman_map.FIRST .. lv_decimal_roman_map.LAST LOOP
-    lv_roman_numeral := lv_roman_numeral || build_roman_numeral_piece( lv_decimal_roman_map(i), lv_decimal_number);
+    lv_roman_numeral := lv_roman_numeral || lv_decimal_roman_map(i).get_roman_numeral_piece(lv_decimal_number);
   END LOOP;
   RETURN lv_roman_numeral;
 END;
