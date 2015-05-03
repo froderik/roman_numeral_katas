d148e0fba53b29bc8d4f7aa00b1fe4e8b31add77
Jacek Gębal <jgebal@gmail.com>
2015-05-03 21:40:48 +0100

    Green

--- sources/to_roman_numeral.sql
+++ sources/to_roman_numeral.sql
@@ -1,6 +1,10 @@
 CREATE OR REPLACE FUNCTION to_roman_numeral( pv_decimal_number INTEGER ) RETURN VARCHAR2 IS
 BEGIN
-  RETURN 'I';
+  RETURN
+    CASE pv_decimal_number
+      WHEN 1 THEN 'I'
+      WHEN 2 THEN 'II'
+    END;
 END;
 /
 
