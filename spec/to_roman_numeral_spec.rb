f4f00f24823e2fc4e5237bf434a17e80bf3f7052
Jacek Gębal <jgebal@gmail.com>
2015-05-03 23:51:04 +0100

    Red

--- spec/to_roman_numeral_spec.rb
+++ spec/to_roman_numeral_spec.rb
@@ -3,7 +3,9 @@ require_relative 'spec_helper'
 describe 'Decimal to roman numeral converter' do
 
   roman_numbers = %w{I II III IV V VI VII VIII IX X XI XII XIII XIV XV XVI XVII XVIII XIX XX
-                    XXI XXII XXIII XXIV XXV XXVI XXVII XXVIII XXIX XXX XXXI XXXII XXXIII XXXIV XXXV XXXVI XXXVII XXXVIII XXXIX XL}
+                     XXI XXII XXIII XXIV XXV XXVI XXVII XXVIII XXIX XXX XXXI XXXII XXXIII XXXIV XXXV XXXVI XXXVII XXXVIII XXXIX XL
+                     XLI
+                    }
   (1..roman_numbers.size).each do |given|
     expected = roman_numbers[given-1]
     it "should return #{expected} for #{given}" do
