0a663ec2d2624b3050ff796cda54c4e4a417a840
Jacek Gębal <jgebal@gmail.com>
2015-05-03 21:39:37 +0100

    Red

--- spec/to_roman_numeral_spec.rb
+++ spec/to_roman_numeral_spec.rb
@@ -6,4 +6,8 @@ describe 'Decimal to roman numeral converter' do
     expect( plsql.to_roman_numeral(1) ).to eq('I')
   end
 
+  it 'should return II for 2' do
+    expect( plsql.to_roman_numeral(2) ).to eq('II')
+  end
+
 end
